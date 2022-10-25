72619b64999e1104570006fe94d4df95e03a0e53
Peace-Olayinka <peaceolayinkaolamilokun@gmai.com>
2022-10-25 13:18:17 +0100

    first commit

---
+++ basicCalculator.js
@@ -0,0 +1,36 @@
+var myDisplay = []
+var myinput
+function myDigit(myNum) {
+    myinput = myNum
+    myDisplay.push(myinput)
+    disp.value = ``
+    for (let index = 0; index < myDisplay.length; index++) { 
+        disp.value += `${myDisplay[index]}`
+    }
+} 
+// symbol
+var myOperator
+function mySymbol(mySym) {
+    myOperator = mySym
+    myDisplay.splice(myDisplay.length,0, myOperator)
+    disp.value = ``
+    for (let index = 0; index < myDisplay.length; index++) { 
+        disp.value += `${myDisplay[index]}`
+    } 
+}
+
+// operation
+ function myEquals() {
+    disp.value =eval(disp.value)
+}
+
+// to clear all 
+function deleteNum() {
+    myDisplay.splice(0, myDisplay.length)
+    disp.value = myDisplay
+}
+
+
+
+
+
